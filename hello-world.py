a1ff980334f7e577a18e190667ff14cafe8d6837
crod1333 <crodr133@ucsc.edu>
2025-09-30 13:10:55 -0700

    adding a new program file
    uploaded our first program

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+def PrintHelloWorld():
+	print("Hello World!")
+
+#we almost always have a 'main' function, which calls other functions 
+#and performs all tasks of our program
+def main():
+	PrintHelloWorld()
+
+#telling the computer that the main function, 'def main', is the first thing you start with
+if __name__=='__main__':
+	main()
+
+# __name__ == __main__ ??
+	# 
+
+# how to run
+	# change directory (cd) where terminal is pulling from (i.e. desktop)
+	#  then you can run the program since it's saved there
+
+
+
